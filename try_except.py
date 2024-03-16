65b9ff932ef9151cc8eaddd36463b0134d6f3cfd
Wanderson-Wendel <wandersonwendel.dev@gmail.com>
2024-03-16 20:36:24 -0300

    try e tipos de exception

---
+++ try_except.py
@@ -0,0 +1,22 @@
+# Try, except, else e finally
+
+try:
+    a = 18
+    b = 0
+    # print(b[0])
+    print('linha 1'[7])
+    c = a / b
+    print('linha 2')
+
+except ZeroDivisionError as error:
+    print('Mensagem:', error) # Divisão por zero
+
+except NameError as error:
+    print('Mensagem:', error) # Nome b não está definido
+
+except (TypeError, IndexError) as error:
+    print('TypeError + IndexError') # TypeError + IndexError
+    print('Mensagem:', error )
+
+except Exception as error:
+    print('Mensagem:', error )
